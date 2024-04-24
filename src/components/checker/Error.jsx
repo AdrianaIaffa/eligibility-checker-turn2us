332a484fd13775985eed75f96974eaf4b5077d0b
AdrianaIaffa <adrianaiaffa@hotmail.com>
2024-04-24 13:28:55 +0100

    added error.jsx file and feedback on age #7

---
+++ src/components/checker/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className="error__content">
+    <h3 className="error__title">I'm sorry, you are not elegible.</h3>
+    <div className="error__info">
+        <p>You must be over 18 to qualify</p>
+    </div>
+</div>
+  )
+}
+
+export default Error
