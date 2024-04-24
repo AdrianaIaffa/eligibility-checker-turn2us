0d73f339c8f11a0e2f319ab93dee83d1c5dcaec5
AdrianaIaffa <adrianaiaffa@hotmail.com>
2024-04-24 12:10:19 +0100

    added title to homepage, removed css from react added component to app.js #1

---
+++ src/components/checker/Checker.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './checker.css'
+
+const Checker= () => {
+
+  return (
+    <section className="checker section" id="checker">
+         <h2 className='section__title'>Benefits Eligibility Checker</h2>
+
+        <div className="checker__container container grid">
+    
+        </div>
+
+    </section>
+  )
+}
+
+export default Checker
