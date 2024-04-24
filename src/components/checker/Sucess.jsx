923a4808c39975d581544eb9a1af91f8de33f37b
AdrianaIaffa <adrianaiaffa@hotmail.com>
2024-04-24 13:10:16 +0100

    added success.jsx file #3

---
+++ src/components/checker/Sucess.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div className="Success__content">
+        <h3 className="success__title">Congratulations, You are elegible!</h3>
+    </div>
+  )
+}
+
+export default Success
